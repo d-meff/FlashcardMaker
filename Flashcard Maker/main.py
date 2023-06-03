b0e1e4987408c04c78973eb9c277fd1e48667c2e
Daniel Mefferd <88276633+d-meff@users.noreply.github.com>
2023-06-02 22:15:41 -0500

    added Testing Game features
    added a game where you can test yourself

--- Flashcard Maker/main.py
+++ Flashcard Maker/main.py
@@ -30,31 +30,34 @@ class MainWin:
         self.viewDeckButton = Button(master, text="View Decks", command=self.go_to_view_deck, background="light green")
         self.viewDeckButton.place(x=265, y=100)
 
-    def go_to_edit_deck(self):
+        self.testGameButton = Button(master, text="Testing Game", command=self.go_to_test_game, background="pink")
+        self.testGameButton.place(x=257, y=130)
+
+    def destroy_elements(self):
         self.testLabel.destroy()
         self.editDeckButton.destroy()
         self.viewDeckButton.destroy()
         self.createDeckButton.destroy()
+        self.testGameButton.destroy()
 
+    def go_to_edit_deck(self):
+        self.destroy_elements()
         self.editDeckNav = EditDeck(root)
     
     def go_to_view_deck(self):
-        self.testLabel.destroy()
-        self.editDeckButton.destroy()
-        self.viewDeckButton.destroy()
-        self.createDeckButton.destroy()
-
+        self.destroy_elements()
         self.viewDeckNav = ViewDeckWin(root)
     
     def go_to_create_deck(self):
-        self.testLabel.destroy()
-        self.editDeckButton.destroy()
-        self.viewDeckButton.destroy()
-        self.createDeckButton.destroy()
-
+        self.destroy_elements()
         self.createDeckNav = CreateDeck(root)
 
-# Editing Window
+    def go_to_test_game(self):
+        self.destroy_elements()
+        self.testGameNav = TestGame(root)
+
+
+# Editing Window --------------------------------------------------------
 class EditDeck:
     def __init__(self, master):
         self.master = master
@@ -171,7 +174,7 @@ class EditDeck:
             self.deckDeletedLabel["text"] = f'Deck {table_name} does not exist.'
 
 
-# Viewing Window
+# Viewing Window -----------------------------------------------------------------------------------------
 class ViewDeckWin:
     def __init__(self, master):
         self.master = master
@@ -427,10 +430,222 @@ class ViewDeckWin:
             self.exportedDeckLabel["text"] = f'{table_name}.csv has been created! (or updated)'
         except:
             self.exportedDeckLabel["text"] = "Error creating deck CSV. (Deck probably doesn't exist)"
+
+# Make Testing Game --------------------------------------------------------------------------------------------------
+class TestGame:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Test yourself!")
+        self.master.geometry("600x500")
+        self.master.minsize(600, 500)
+        self.master.maxsize(600, 500)
+
+        self.backButton = Button(master, text="Back to Main Menu", command=self.back_to_menu, background="pink")
+        self.backButton.place(x=10, y=10)
+
+        self.tableLoadLabel = Label(master, text="Table to load:")
+        self.tableLoadLabel.place(x=160, y=50)
+
+        self.tableLoadEntry = Entry(master, width=40)
+        self.tableLoadEntry.place(x=245, y=50)
+
+        self.tableQuestionText = Label(master)
+        self.tableQuestionText.place(x=150, y=150)
+
+        self.answerChoiceButtonOne = Button(master, text="1", command=self.one_clicked, padx=10, pady=10)
+        self.answerChoiceButtonTwo = Button(master, text="2", command=self.two_clicked, padx=10, pady=10)
+        self.answerChoiceButtonThree = Button(master, text="3", command=self.three_clicked, padx=10, pady=10)
+        self.answerChoiceButtonFour = Button(master, text="4", command=self.four_clicked, padx=10, pady=10)
+
+        self.answerChoiceButtonOne.place(x=100, y=205)
+        self.answerChoiceButtonTwo.place(x=100, y=305)
+        self.answerChoiceButtonThree.place(x=350, y=205)
+        self.answerChoiceButtonFour.place(x=350, y=305)
+
+        self.loadTableButton = Button(master, text="Load Table", command=self.load_table_questions, background="pink")
+        self.loadTableButton.place(x=280, y=75)
+
+        self.newQuestionButton = Button(master, text="New Question", command=self.load_random_question, background="pink")
+        self.newQuestionButton.place(x=272, y=418)
+
+    def load_random_question(self):
+        global data_rows
+        global count_row
+        global rows
+        global card_val
+
+        if len(data_rows) < 5:
+            self.tableQuestionText["text"] = "error, must have more than 5 cards to play the Testing Game"
+        else:
+            count_row = random.randint(0, len(data_rows) - 1)
+            card_val = count_row + 1
+            self.set_buttons()
+
+    def set_buttons(self):
+        self.tableQuestionText["text"] =  f'{rows[count_row][1]}'
+
+        self.answerChoiceButtonOne["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+        self.answerChoiceButtonTwo["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+        self.answerChoiceButtonThree["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+        self.answerChoiceButtonFour["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+
+        self.duplicate_check()
+
+        # resetting colors
+
+        self.answerChoiceButtonOne["background"] = "gray"
+        self.answerChoiceButtonTwo["background"] = "gray"
+        self.answerChoiceButtonThree["background"] = "gray"
+        self.answerChoiceButtonFour["background"] = "gray"
+
+    def load_table_questions(self):
+        global data_rows
+        global count_row
+        global rows
+        global card_val
+
+        global buttonOneClicked
+        global buttonTwoClicked
+        global buttonThreeClicked
+        global buttonFourClicked
+
+        buttonOneClicked = False
+        buttonTwoClicked = False
+        buttonThreeClicked = False
+        buttonFourClicked = False
+
+        table_name = self.tableLoadEntry.get()
         
+        try:
+            card_val = 1
+
+            rows = []
+            count_row = 0
+
+            c.execute(f'''SELECT * FROM {table_name}''')
+            data_rows = c.fetchall()
+
+            if len(data_rows) < 5:
+                self.tableQuestionText["text"] = "error, must have more than 5 cards to play the Testing game"
+            else:
 
 
-# Creating Window
+                for row in data_rows:
+                    rows.append(row)
+
+                self.set_buttons()
+            
+        except:
+            print("error")
+    
+    def duplicate_check(self):
+            comparisons = [self.answerChoiceButtonOne["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonFour["text"]]
+
+            
+            comparison_flag = comparisons.count(True)
+            
+            while comparison_flag != 0:
+                self.answerChoiceButtonOne["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+                self.answerChoiceButtonTwo["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+                self.answerChoiceButtonThree["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+                self.answerChoiceButtonFour["text"] = f'{rows[random.randint(0, len(data_rows) - 1)][2]}'
+
+                comparisons = [self.answerChoiceButtonOne["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonFour["text"]]
+
+                comparison_flag = comparisons.count(True)
+
+                
+            buttons = [self.answerChoiceButtonOne, self.answerChoiceButtonTwo, 
+                    self.answerChoiceButtonThree, self.answerChoiceButtonFour]
+                
+            buttons[random.randint(0, 3)]["text"] = f'{rows[count_row][2]}'
+
+            comparisons = [self.answerChoiceButtonOne["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonOne["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonThree["text"],
+                           self.answerChoiceButtonTwo["text"] == self.answerChoiceButtonFour["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonOne["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonTwo["text"],
+                           self.answerChoiceButtonThree["text"] == self.answerChoiceButtonFour["text"]]
+
+            if comparisons.count(True) != 0:
+                self.duplicate_check()
+                
+
+        
+    def one_clicked(self):
+        global buttonOneClicked
+        buttonOneClicked = True
+        self.check_answer()
+    
+    def two_clicked(self):
+        global buttonTwoClicked
+        buttonTwoClicked = True
+        self.check_answer()
+
+    def three_clicked(self):
+        global buttonThreeClicked
+        buttonThreeClicked = True
+        self.check_answer()
+    
+    def four_clicked(self):
+        global buttonFourClicked
+        buttonFourClicked = True
+        self.check_answer()
+
+    def check_answer(self):
+        global buttonOneClicked
+        global buttonTwoClicked
+        global buttonThreeClicked
+        global buttonFourClicked
+
+        vals = [[buttonOneClicked, self.answerChoiceButtonOne], 
+                [buttonTwoClicked, self.answerChoiceButtonTwo],
+                [buttonThreeClicked, self.answerChoiceButtonThree],
+                [buttonFourClicked, self.answerChoiceButtonFour]]
+        
+        for button in vals:
+            if button[1]["text"] == f'{rows[count_row][2]}':
+                button[1]["background"] = "green"
+            else:
+                button[1]["background"] = "red"
+                
+
+        
+    def back_to_menu(self):
+        self.backButton.destroy()
+        self.tableLoadEntry.destroy()
+        self.tableLoadLabel.destroy()
+        self.tableQuestionText.destroy()
+        self.loadTableButton.destroy()
+        self.answerChoiceButtonOne.destroy()
+        self.answerChoiceButtonTwo.destroy()
+        self.answerChoiceButtonThree.destroy()
+        self.answerChoiceButtonFour.destroy()
+        self.newQuestionButton.destroy()
+
+        self.mainMenuNav = MainWin(root)
+
+
+# Creating Window ------------------------------------------------------------------------------------
 class CreateDeck:
     def __init__(self, master):
         self.master = master
